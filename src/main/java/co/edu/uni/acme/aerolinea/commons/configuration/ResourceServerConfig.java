546f9338c7a6ef161544d206945bd304aff4707c
MariaFernanda1818 <mariafvn0127@gmail.com>
2025-03-23 18:15:59 -0500

    Se ajusta al BCrypt

--- src/main/java/co/edu/uni/acme/aerolinea/commons/configuration/ResourceServerConfig.java
+++ src/main/java/co/edu/uni/acme/aerolinea/commons/configuration/ResourceServerConfig.java
@@ -6,6 +6,8 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,6 +23,11 @@ public class ResourceServerConfig {
                 )
                 .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
