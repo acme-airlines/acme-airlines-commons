d6095e0c8105843e2b6b2e2f7d2e8c6c85d610b1
MariaFernanda1818 <83258884+MariaFernanda1818@users.noreply.github.com>
2025-04-29 10:05:54 -0500

    Custom entity

--- src/main/java/co/edu/uni/acme/aerolinea/commons/entity/FlightCityEntity.java
+++ src/main/java/co/edu/uni/acme/aerolinea/commons/entity/FlightCityEntity.java
@@ -10,15 +10,15 @@ public class FlightCityEntity {
 
     @Id
     @Column(name = "code_flight_city")
-    private String codeFlightCity;
+    private String flightCityCode;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "code_flight_fk", referencedColumnName = "code_flight")
-    private FlightEntity codeFlightFk;
+    private FlightEntity flight;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "code_city_fk", referencedColumnName = "code_city")
-    private CityEntity codeCityFk;
+    private CityEntity city;
 
     @Column(name = "number_flight")
     private String numberFlight;
