df5e313820b06dbb10a9eebd72435a408753d351
MariaFernanda1818 <83258884+MariaFernanda1818@users.noreply.github.com>
2025-05-06 18:47:01 -0500

    Custom dto's and entitys

--- src/main/java/co/edu/uni/acme/aerolinea/commons/dto/UserDTO.java
+++ src/main/java/co/edu/uni/acme/aerolinea/commons/dto/UserDTO.java
@@ -17,24 +17,24 @@ public class UserDTO {
     private String codeUser;
 
     @NotBlank(message = "El nombre del pasajero es obligatorio")
-    private String namePassenger;
+    private String nameUser;
 
     @NotBlank(message = "El apellido del pasajero es obligatorio")
-    private String lastNamePassenger;
+    private String lastNameUser;
 
     @NotBlank(message = "El número de teléfono es obligatorio")
     @Pattern(regexp = "\\d{7,15}", message = "El número de teléfono debe tener entre 7 y 15 dígitos")
-    private String phonePassenger;
+    private String phoneUser;
 
     @NotNull(message = "El tipo de documento es obligatorio")
-    private DocumentTypeDTO documentTypePassengerFk;
+    private DocumentTypeDTO documentTypeUserFk;
 
     @NotBlank(message = "El número de documento es obligatorio")
-    private String numberDocumentPassenger;
+    private String numberDocumentUser;
 
     @NotBlank(message = "El correo electrónico es obligatorio")
     @Email(message = "El correo electrónico debe ser válido")
-    private String emailPassenger;
+    private String emailUser;
 
     @NotNull(message = "La fecha de nacimiento es obligatoria")
     @Past(message = "La fecha de nacimiento debe ser en el pasado")
@@ -44,7 +44,7 @@ public class UserDTO {
 
     @NotBlank(message = "El género es obligatorio")
     @Pattern(regexp = "M|F", message = "El género debe ser M, F")
-    private String genderPassenger;
+    private String genderUser;
 
     private String hashPassword;
 
