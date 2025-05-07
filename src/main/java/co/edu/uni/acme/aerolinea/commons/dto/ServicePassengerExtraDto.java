e747b808baddc2765ad077a7aff1bec6f3e3fedc
MariaFernanda1818 <83258884+MariaFernanda1818@users.noreply.github.com>
2025-05-07 12:13:07 -0500

    dto and pom adjustment

--- src/main/java/co/edu/uni/acme/aerolinea/commons/dto/ServicePassengerExtraDto.java
+++ src/main/java/co/edu/uni/acme/aerolinea/commons/dto/ServicePassengerExtraDto.java
@@ -1,9 +1,25 @@
 package co.edu.uni.acme.aerolinea.commons.dto;
 
+import co.edu.uni.acme.aerolinea.commons.entity.PassengerEntity;
+import co.edu.uni.acme.aerolinea.commons.entity.ServiceEntity;
+import co.edu.uni.acme.aerolinea.commons.entity.ServicePassengerExtraEntity;
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
 
 @Data
 public class ServicePassengerExtraDto {
-    private String passengerCode;
-    private String serviceCode;
+    public static class Id implements Serializable {
+        private String codePassenger;
+
+        private String codeService;
+    }
+
+    private Id id;
+
+    private PassengerDto passenger;
+
+    private ServiceDTO service;
 }
